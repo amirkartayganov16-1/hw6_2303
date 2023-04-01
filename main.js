767f83fd56615fd8ac443e6129e1d82fcfafcd3a
amirkart <100897206+amirkartayganov16-1@users.noreply.github.com>
2023-04-01 12:02:13 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,35 @@
+const block = document.querySelector('.block');
+const buttons = document.querySelector('.buttons');
+let count = 1;
+
+buttons.addEventListener('click', (event) => {
+    const button = event.target;
+    if (button.classList.contains('next') && count < 200) {
+        count++;
+        fetchData(count);
+    } else if (button.classList.contains('prev') && count > 1) {
+        count--;
+        fetchData(count);
+    }
+});
+
+function fetchData(count) {
+    fetch(`https://jsonplaceholder.typicode.com/todos/${count}`)
+        .then(response => response.json())
+        .then(data => {
+            block.innerHTML = `
+                <h2>${data.title}</h2>
+                <p>ID: ${data.id}</p>
+                <p>Completed: ${data.completed}</p>
+            `;
+        });
+}
+
+fetch('https://jsonplaceholder.typicode.com/posts')
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+    })
+    .catch(error => {
+        console.error(error);
+    });
